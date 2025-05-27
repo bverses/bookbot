11ff5a3a474e3fb9faf33a1b14139ca732ccc89f
bverses.user0 <bverses@dpazcesar@gmail.com>
2025-05-26 19:16:06 -0700

    Bookbot: command line arguements added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import *
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,19 +8,26 @@ def get_book_text(filepath):
 
 
 def main():
-    text_block = get_book_text("books/frankenstein.txt")
-    #print(text)
-    word_count = get_num_words(text_block)
-    #print(count)
-    
-    char_count = get_char_count(text_block)
-    #print(char_count)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        return sys.exit(1)
+    else:
+        filepath = sys.argv[1]
 
-    #OUTPUT =============================================
-    print("============ BOOKBOT ============" '\n' "Analyzing book found at books/frankenstein.txt...")
-    print(f"----------- Word Count ---------- \n{word_count}")
-    print("--------- Character Count -------")
-    print_report(char_count)
-    print("============= END ===============")
+        text_block = get_book_text(filepath)
+        #print(text)
+        word_count = get_num_words(text_block)
+        #print(count)
+        
+        char_count = get_char_count(text_block)
+        #print(char_count)
+
+        #OUTPUT =============================================
+        print("============ BOOKBOT ============" '\n' "Analyzing book found at books/frankenstein.txt...")
+        print(f"----------- Word Count ---------- \n{word_count}")
+        print("--------- Character Count -------")
+        print_report(char_count)
+        print("============= END ===============")
+    return sys.exit(0)
 
 main()
